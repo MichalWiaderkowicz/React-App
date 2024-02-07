f9fea9dff4f5578a06581c8cda058232f3e2fefb
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-02-07 12:32:07 +0100

    add utils

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,12 +2,11 @@ import styles from './Column.module.scss';
 import Card from '../Card/Card.js';
 import CardForm from '../CardForm/CardForm.js';
 import { useSelector } from 'react-redux';
+import { getFilteredCards } from '../../redux/store';
 
 
 const Column = props => {
-    const searchString = useSelector( state => state.searchString);
-    const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id && card.title.toLowerCase().includes(searchString.toLowerCase())));
-    console.log('cards', cards)
+    const cards = useSelector(state => getFilteredCards(state, props.id));
     return (
         <article className={styles.column}>
             <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon} />{props.title}</h2> 
