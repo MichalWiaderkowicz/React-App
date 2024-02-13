d432e6c62228491a016a5029f09daa447d9d24d8
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-02-13 16:51:06 +0100

    add notFound

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import NavBar from './components/NavBar/NavBar.js';
 import Home from './components/Home/Home.js';
 import Favorite from './components/Favorite/Favorite.js';
 import About from './components/About/About.js'
+import { Routes, Route } from 'react-router-dom';
+import NotFound from './components/NotFound/NotFound.js';
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Container>
-        <Home />
-        <Favorite />
-        <About />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/favorite" element={<Favorite />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </Container>
     </main>
   );
